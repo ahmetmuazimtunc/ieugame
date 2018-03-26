1c0481625c2cac5a14bced9eafffc43ca7052b07
Mesutcan Göksoy <mgksoy@gmail.com>
2018-03-26 19:48:59 +0300

    26.03.18
    Custom ogt joystick scripti yazildi. joystick sprite'ından aci hesabi yapip , karakterimizin 3d dünyasina iz dusumunu aliyor.
    Bazi yerlerde yorum satirlari da var. Merak edenler inceleyeiblir.
    Her satira yorum satiri koyamadım. Anlamadiginiz bir yer olursa sorabilirsiniz.

---
+++ IEU Game Project/Assets/Scripts/Player/OGTCharacterMovement.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class OGTCharacterMovement : NetworkBehaviour
+{   
+    [SerializeField]
+    private Rigidbody _rigidBody;
+
+    [SerializeField]
+    private Animator _animator;
+
+    [SerializeField]
+    GameObject CameraPrefab = null;
+
+    private static OGTCharacterMovement _singleton;
+
+    public static OGTCharacterMovement Singleton
+    {
+        get { return _singleton; }
+    }
+
+    private const float _directionUnit = 1.0f;
+
+    //Karaktermizin hareket edip etmeyecegini, edecekse hangi yone edecegini buradan anliyoruz. Hareket istenmiyorsa 0'lar atanmali
+    private Vector3 _direction = new Vector3(0, 0, 0);
+
+    private int _hashJump;
+    private int _hashRun;
+    private int _hashIdle;
+
+    private void Start()
+    {
+        if (_singleton == null)
+        {
+            _singleton = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+        _hashIdle = Animator.StringToHash("idle");
+        _hashJump = Animator.StringToHash("jump");
+        _hashRun = Animator.StringToHash("run");
+
+        //_directionOfMovement = transform.position + 
+    }
+
+    private void Movecharacter()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        if (_direction == Vector3.zero)
+        {
+            return;
+        }
+
+        if (!_animator.GetBool(_hashRun))
+        {
+            _animator.SetBool(_hashRun, true);
+        }
+
+        transform.Translate(_direction * Time.fixedDeltaTime);
+    }
+
+    /// <param name="joystickAngle">Joystick'ten gelen aci gosteren Vektor</param>
+    public void SetDirectionFromJoystickAngle(float joyStickAngle)
+    {
+        _direction = Quaternion.AngleAxis(joyStickAngle, Vector3.forward) * _direction;
+        //Debug.Log(_direction);
+    }
+
+    private void FixedUpdate()
+    {
+        Movecharacter();
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GameObject CopyCamera = GameObject.Instantiate(CameraPrefab, transform, false);
+    }
+}
+
+public static class OgtMathHelper 
+{
+    /// <summary>
+    /// iki vektorumuz arasindaki aciyi hesaplamamiz gereken durumlarda gorsel olarak yardımcı olacak bir fonksiyon
+    /// Parametre olarak verdigimiz iki vektoru Unity Editor icerisinde Scene'e cizdiriyor ve aralarindaki aciyi Konsol penceresine yazdiriyor.
+    /// </summary>
+    public static void DebugAngleAndVisualizeVectors(Vector3 vectorOne, Vector3 vectorTwo)
+    {
+        Debug.DrawRay(Vector3.zero, vectorOne);
+        Debug.DrawRay(Vector3.zero, vectorTwo);
+        float angle = CalcualteAngleBetweenTwoVectors(vectorOne, vectorTwo);
+
+        Debug.Log(angle);
+    }
+
+    public static float CalcualteAngleBetweenTwoVectors(Vector3 vectorOne, Vector3 vectorTwo)
+    {
+        return Vector3.Angle(vectorOne, vectorTwo);
+    }
+
+    public static float ConvertToPositive(float number)
+    {
+        float toReturn = number;
+        toReturn = (toReturn < 0) ? (-toReturn) : (toReturn);
+        return toReturn;
+    }    
+}
---
+++ IEU Game Project/Assets/Scripts/UI/OGTJoystick.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UnityStandardAssets.CrossPlatformInput
+{
+    public class OGTJoystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+    {
+        public int _halfRadius = 100;
+
+        [SerializeField]
+        private Transform _origin = null;
+
+        ////custom Koordinat sistemimizi cizdirmek icin ne kadar kaydiracagiz orjinal koordinat sistemi merkezininin x'ini
+        //[SerializeField]
+        //private float _coorXOffset = 0f;
+
+        ////custom Koordinat sistemimizi cizdirmek icin ne kadar kaydiracagiz orjinal koordinat sistemi merkezininin y'sini
+        //[SerializeField]
+        //private float _coorYOffset = 0f;
+
+        void Start()
+        {
+            //_coorXOffset = _origin.position.x / 2;
+            //_coorYOffset = _origin.position.y / 2;
+        }
+
+        void Update()
+        {
+            DrawCoordinateSystem();
+            DrawJoystickRadiusAndVectors();
+        }
+
+        //joystickimizin sinirlarini gosteren kareyi View'a cizdiren method
+        private void DrawJoystickRadiusAndVectors()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                //Kare kenarlarımızın koordinat noktalari
+                float xOne, yOne;
+                //eger ilk veya son kenar ise; x = orjin - bir kenar / 2
+                if (i == 0 | i == 3)
+                {
+                    xOne = _origin.position.x - _halfRadius; 
+                }
+                else
+                {
+                    xOne = _origin.position.x + _halfRadius;
+                }
+                //eger 1. ve 2. kenari cizidiriyorsak (yani index 0 ve 1)
+                if (i <= 1)
+                {
+                    //kenarlarin baslangic noktasi ayni ve karenin ust yarisinda olacak
+                    yOne = _origin.position.y + _halfRadius; 
+                }
+                else//3. ve 4. kenari cizidiriyorsak
+                {
+                    //kenarlarin baslangic noktasi ayni ve karenin alt kisminda olacak
+                    yOne = _origin.position.y - _halfRadius;
+                }
+                //cizginin ilk noktasi
+                Vector3 CoordinateOne = new Vector3(xOne, yOne, 0);
+                //cizginin ikinci noktasi
+                Vector3 CoordinateTwo = new Vector3(CoordinateOne.x, CoordinateOne.y, 0);
+
+                //eger i cift ise yani 0 veya 2 ise; yani 1. veya 3. kenarlar cizilmekte ise
+                if (i % 2 == 0)
+                {
+                    //1. kenar ise toplayarak bir kenar uzunlugu ekle 
+                    //2.kenar ise cikararak bir kenar uzunlugu ekle 
+                    CoordinateTwo.x = xOne + ((i == 2) ? (-(_halfRadius * 2)) : (_halfRadius * 2));
+                }
+                else //eger tek ise
+                {
+                    //y'sine bir kenar uzunlugu ekle (2r)
+                    //ama i == 1 ise cikararak , 3 ise toplayarak ekle
+                    CoordinateTwo.y = yOne + ((i == 1) ? (-_halfRadius * 2) : (_halfRadius * 2));
+                }
+                Debug.DrawLine(CoordinateOne, CoordinateTwo);
+            }
+        }
+
+        //Aci hesabinda kullandiğimiz koordinat sistemimizi ekrana cizdiren method
+        private void DrawCoordinateSystem()
+        {
+            Debug.DrawLine(new Vector3(_origin.position.x - _halfRadius * 2, _origin.position.y, 0), new Vector3(_origin.position.x + _halfRadius * 2, _origin.position.y, 0));
+            Debug.DrawLine(new Vector3(_origin.position.x, _origin.position.y - _halfRadius * 2, 0), new Vector3(_origin.position.x, _origin.position.y + _halfRadius * 2, 0));
+        }
+
+        public void OnDrag(PointerEventData data)
+        {
+            transform.position = Input.mousePosition;
+            float currentX = transform.position.x;
+            float currentY = transform.position.y;
+            //x kontrol
+            if (currentX > _origin.position.x + _halfRadius)
+            {
+                currentX = _origin.position.x + _halfRadius;
+            }
+            else if (currentX < _origin.position.x - _halfRadius)
+            {
+                currentX = _origin.position.x - _halfRadius;
+            }
+            //y kontrol
+            if (currentY > OgtMathHelper.ConvertToPositive(_origin.position.y + _halfRadius))
+            {
+
+                currentY = _origin.position.y + _halfRadius;
+            }
+            else if (currentY < _origin.position.y - _halfRadius)
+            {
+                currentY = _origin.position.y - _halfRadius;
+            }
+
+            transform.position = new Vector3(currentX, currentY);
+
+            //RenderAngle(); //BURADA KALDIN .... ACİLARİ DA ALDIR BİTSİN!...
+        }
+
+        /// <summary>
+        /// iki vektoru cizip acisini aliyor
+        /// </summary>
+        private void RenderAngle(Vector3 vectorOne , Vector3 vectorTwo)
+        {
+            OgtMathHelper.DebugAngleAndVisualizeVectors(vectorOne, vectorTwo);
+        }
+
+        public void OnPointerUp(PointerEventData data)
+        {
+            Debug.Log("up triggered");
+            transform.position = _origin.position;
+        }
+
+
+        public void OnPointerDown(PointerEventData data)
+        {
+
+        }
+    }
+}
