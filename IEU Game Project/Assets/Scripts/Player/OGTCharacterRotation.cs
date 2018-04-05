333da7ab9b414c63776d7e099fdd8404ac75b723
mesutcan göksoy <mgksoy@gmail.com>
2018-04-05 18:38:22 +0300

    05.04.2018
    characterRotation.cs tamamlandı. Ileri projelerde de disaridan herhangi bir aci degiskeni ile herhangi bir oyun objesini dondurmek amacli kullanilabilir.

--- IEU Game Project/Assets/Scripts/Player/OGTCharacterRotation.cs
+++ IEU Game Project/Assets/Scripts/Player/OGTCharacterRotation.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-//bu class karakter objesinin bir component'ı olmalı
+//1-Bu class karakter objesinin bir component'ı olmalı
+//2-Adi Joystick olan aci degiskenine istediginiz hardware'den aci vererek de kullanabilirsiniz...
+//...Joystick haricindeki aygitlardan aci alarak da calisir bu class.
 public class OGTCharacterRotation : NetworkBehaviour
 {
     private OGTCharacterRotation _singleton;
@@ -48,36 +50,43 @@ public class OGTCharacterRotation : NetworkBehaviour
         //joystick ile karakterin bakis yonu arasinda acisal fark var mi?
         float StickAngleY = OGTCharacterMovement.Singleton.DirectionPointAngle;
         float CharAngleY = Angle_Char_yAxis;
-        float AngleDifferenceSigned = StickAngleY - CharAngleY;        
+        //joystick aci - karakter aci (signed)
+        float StickMinusCharSigned = StickAngleY - CharAngleY;
+        //joystick ile karakter arasindaki aci (unsigned - pozitif)
+        float StickMinusChar = OgtMathHelper.ConvertToPositive(StickMinusCharSigned);
         //eger bu script; Client'imizin karakteri uzerinde calismiyorsa, yani diger oyuncularin karakterlerinden birisi ise;
         //VEYA karakter ile joystick ayni aciya bakiyorsa
-        if (!isLocalPlayer || StickAngleY == 0f /*|| AngleDifference < _angleAmountToStartRotating */)
+        if (!isLocalPlayer || StickAngleY == 0f || StickMinusChar < _angleAmountToStartRotating)
         {
             //bu fonksiyonun bu satirdan daha altindaki satirlarini okuma
             return;
         }        
         //bir framede donecegi aci
-        float angle = _anglePerFrameFixed;
-        //joystick ile karakter arasindaki acinin pozitifi
-        float AngleDifference = OgtMathHelper.ConvertToPositive(AngleDifferenceSigned);
+        float angle = _anglePerFrameFixed;        
         //aralarindaki aciyi 360'a tamamlayan aci
-        float RemainingAngle = 360 - AngleDifference;
-        //hangi aci daha kisa donmek icin?
-        if (AngleDifference <= RemainingAngle)
-        {
+        float RemainingAngle = 360 - StickMinusChar;
 
-        }
-        else
+        //hangi aci daha kisa donmek icin? Bu kosul
+        if(StickMinusChar <= RemainingAngle)
         {
-
+            //eger aralarindaki aci eksi ciktiysa, karakter joystickin saginda demektir
+            if (StickMinusCharSigned < 0)
+            {
+                //karakterin sola donmesi icin, bakis yonu acisini azaltmamiz gerekiyor
+                angle = -angle;
+            }
         }
-
-        //eger aralarindaki aci eksi ciktiysa, karakter joystickin solunda demektir
-        if (AngleDifferenceSigned < 0)
+        else //360a tamamlayan diger aci daha kisa ise;
         {
-            //karakterin saga donmesi icin, bakis yonu acisini azaltmamiz gerekiyor
-            angle = -angle;
+            //ve aralarindaki aci pozitif ise , yani karakter joystickin solunda ise;
+            if (StickMinusCharSigned > 0)
+            {
+                //karakterin saga donmesi icin, bakis yonu acisini arttirmamiz gerekiyor
+                angle = -angle;
+            }
         }
+
+
         _character.Rotate(Vector3.up, angle);
     }
 
